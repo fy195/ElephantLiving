0e1c99d5411e5f0b6afb42badab331cee99548f7
Omaiga <lc_522@163.com>
2016-10-26 11:12:56 +0800

    ignore add ijkplayerLib

---
+++ ElephantLiving/ElephantLiving/Vendors/ElPlaceholderViewController.h
@@ -0,0 +1,13 @@
+//
+//  ElPlaceholderViewController.h
+//  ElephantLiving
+//
+//  Created by Omaiga on 16/10/26.
+//  Copyright © 2016年 dllo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ElPlaceholderViewController : UIViewController
+
+@end
