ce233e40d1f4806197f23a95c060d530b01212ec
fy <fy_195@163.com>
2016-10-26 09:14:41 +0800

    2016.10.26 9:14

---
+++ ElephantLiving/ElephantLiving/Modules/Watch/ElWatchViewController.h
@@ -0,0 +1,13 @@
+//
+//  ElWatchViewController.h
+//  ElephantLiving
+//
+//  Created by dllo on 16/10/25.
+//  Copyright © 2016年 dllo. All rights reserved.
+//
+
+#import "ElBaseViewController.h"
+
+@interface ElWatchViewController : ElBaseViewController
+
+@end
