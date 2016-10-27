d7fde8cdfa59296ddff19004daf59302b720367d
Sakata_ZK <Sakata_ZK@163.com>
2016-10-27 13:42:21 +0800

    2016.10.27 13:42

---
+++ ElephantLiving/ElephantLiving/Modules/Live/View/ElLivingBottomToolView.m
@@ -0,0 +1,21 @@
+//
+//  ElLivingBottomToolView.m
+//  ElephantLiving
+//
+//  Created by dllo on 16/10/27.
+//  Copyright © 2016年 dllo. All rights reserved.
+//
+
+#import "ElLivingBottomToolView.h"
+
+@implementation ElLivingBottomToolView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ ElephantLiving/ElephantLiving/Modules/Live/View/ElLivingTopView.h
@@ -0,0 +1,15 @@
+//
+//  ElLivingTopView.h
+//  ElephantLiving
+//
+//  Created by dllo on 16/10/27.
+//  Copyright © 2016年 dllo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ElLivingTopView : UIView
+
++ (instancetype)elLivingTopView;
+
+@end
