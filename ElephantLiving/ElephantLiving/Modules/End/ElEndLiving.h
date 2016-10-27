426ca7e9ea6fb37eb03dd633a952c5eb57111d8b
Omaiga <lc_522@163.com>
2016-10-27 11:12:34 +0800

    2016.10.27 11:12

---
+++ ElephantLiving/ElephantLiving/Modules/End/ElEndLiving.h
@@ -0,0 +1,27 @@
+//
+//  ElEndLiving.h
+//  ElephantLiving
+//
+//  Created by Omaiga on 16/10/27.
+//  Copyright © 2016年 dllo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ElEndLiving : UIView
+
+@property (nonatomic, strong)UILabel *endLabel;
+
+@property (nonatomic, strong)UILabel *timeTextLabel;
+
+@property (nonatomic, strong)UILabel *timeLabel;
+
+@property (nonatomic, copy)NSString *time;
+
+@property (nonatomic, strong)UILabel *viewTextLabel;
+
+@property (nonatomic, strong)UILabel *viewLabel;
+
+@property (nonatomic, copy)NSString *view_count;
+
+@end
