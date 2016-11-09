51cae926068a63604089dd504224843199129429
Sakata_ZK <Sakata_ZK@163.com>
2016-11-09 11:21:10 +0800

    举报页面弹出

--- ElephantLiving/ElephantLiving/Modules/Watch/Controller/ElWatchViewController.m
+++ ElephantLiving/ElephantLiving/Modules/Watch/Controller/ElWatchViewController.m
@@ -415,7 +415,7 @@ ElLivingTopViewDelegate
 
 - (void)report {
     ElReportViewController *reportViewController = [[ElReportViewController alloc] init];
-    [self.navigationController pushViewController:reportViewController animated:YES];
+    [self presentViewController:reportViewController animated:YES completion:nil];
 }
 
 - (void)follow:(BOOL)isFollow {
